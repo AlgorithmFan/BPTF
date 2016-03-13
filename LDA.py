f69629b56b9bde71e7526e039bdcebec09c863b8
zhang.haidong <haidong_zhang13@163.com>
2016-03-13 20:41:11 +0800

    Latent Dirichlet Allocation

--- LDA.py
+++ LDA.py
@@ -1,9 +1,15 @@
 #!usr/bin/env python
 #coding:utf-8
 
+import sys
+import numpy as np
+
 
 class LDA:
-    def __init__(self):
+    def __init__(self, cf):
+        self.cf = cf
+
+    def fit(self):
         pass
 
 
